b13632789c4bd35447c080184f9bbe2d8f6d35ab
eduardo-zup <eduardo.bento@zup.com.br>
2021-07-19 11:28:17 -0300

    add nova proposta request

--- src/main/java/com/desafio/zup/proposta/proposta/NovaPropostaController.java
+++ src/main/java/com/desafio/zup/proposta/proposta/NovaPropostaController.java
@@ -1,14 +1,17 @@
 package com.desafio.zup.proposta.proposta;
 
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("propostas")
 public class NovaPropostaController {
 
     @PostMapping
-    public void nova(){
+    public void nova(@RequestBody @Valid NovaPropostaRequest request){
     }
 }
---
+++ src/main/java/com/desafio/zup/proposta/proposta/NovaPropostaRequest.java
@@ -0,0 +1,36 @@
+package com.desafio.zup.proposta.proposta;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PositiveOrZero;
+import java.math.BigDecimal;
+
+public class NovaPropostaRequest {
+
+    @NotBlank
+    private String documento;
+
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    private String endereco, nome;
+
+    @NotNull
+    @PositiveOrZero //salario pode ser zero?
+    private BigDecimal salario;
+
+    public NovaPropostaRequest(@NotBlank String documento,
+                               @NotBlank @Email String email,
+                               @NotBlank String endereco,
+                               @NotBlank String nome,
+                               @NotNull @PositiveOrZero BigDecimal salario) {
+        this.documento = documento;
+        this.email = email;
+        this.endereco = endereco;
+        this.nome = nome;
+        this.salario = salario;
+    }
+}
