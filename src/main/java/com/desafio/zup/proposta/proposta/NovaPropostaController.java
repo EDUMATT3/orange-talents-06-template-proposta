bce5634197076661074051c5557e38c1bb0309a4
eduardo-zup <eduardo.bento@zup.com.br>
2021-07-20 12:03:44 -0300

    add json log

--- src/main/java/com/desafio/zup/proposta/proposta/NovaPropostaController.java
+++ src/main/java/com/desafio/zup/proposta/proposta/NovaPropostaController.java
@@ -1,5 +1,7 @@
 package com.desafio.zup.proposta.proposta;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,12 +22,16 @@ public class NovaPropostaController {
     @PersistenceContext
     private EntityManager em;
 
+    private final Logger logger = LoggerFactory.getLogger(NovaPropostaController.class);
+
     @PostMapping
     @Transactional
     public ResponseEntity<?> nova(@RequestBody @Valid NovaPropostaRequest request, UriComponentsBuilder builder){
 
         Proposta novaProposta = request.toModel();
         em.persist(novaProposta);
+
+        logger.info("Proposta criada com {}", novaProposta.getId());
 
         URI location = builder.path("/propostas/{id}").build(novaProposta.getId());
         return ResponseEntity.created(location).build();
