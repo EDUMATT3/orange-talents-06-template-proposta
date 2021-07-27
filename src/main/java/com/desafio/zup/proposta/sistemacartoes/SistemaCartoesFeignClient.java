a1d530aae4b5a3d5253fedd62f668f2862a8b468
eduardo-zup <eduardo.bento@zup.com.br>
2021-07-27 13:08:06 -0300

    add notificar sistemas legado

--- src/main/java/com/desafio/zup/proposta/sistemacartoes/SistemaCartoesFeignClient.java
+++ src/main/java/com/desafio/zup/proposta/sistemacartoes/SistemaCartoesFeignClient.java
@@ -1,12 +1,21 @@
 package com.desafio.zup.proposta.sistemacartoes;
 
+import feign.Response;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Map;
 
 @FeignClient(name = "sistema-cartoes", url = "${api.cartoes.url}")
 public interface SistemaCartoesFeignClient {
 
     @GetMapping
     SistemaCartoesResponse getCartaoByIdProposta(@RequestParam Long idProposta);
+
+    @PostMapping("{id}/bloqueios")
+    Response nofiticarBloqueio(@PathVariable String id, Map<String, String> body);
+
 }
